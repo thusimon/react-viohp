59f2fc3495ab7c98009c871c1a6c7f4f09a3b2a7
Lu <thusimon@gmail.com>
2019-10-28 23:11:38 -0700

    fix non reference error

--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = new express.Router();
 const auth = require('./middleware/auth');
-
+const User = require('../models/user');
 
 router.get('/api/user/me',  async (req, res) => {
   try {
