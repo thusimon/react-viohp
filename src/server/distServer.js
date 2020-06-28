130401fc86d3bd51e496a5e93086faa1540d031d
Lu.Xu <thusimon@gmail.com>
2020-06-28 01:53:43 -0700

    unblock heroku server start up

--- src/server/distServer.js
+++ src/server/distServer.js
@@ -11,19 +11,20 @@ const db = require('./db/mongoose');
 app.use(compression());
 app.use(express.static(path.join(__dirname, '../../dist')));
 
+app.get('*', function(req, res){
+  return res.sendFile(path.join(__dirname, '../../dist/index.html'));
+});
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+
+server.listen(port, function(err){
+  if(err){
+    console.log(err);
+  }
+});
 
 db.connectToDb(false)
 .then(() => {
-  app.get('*', function(req, res){
-    return res.sendFile(path.join(__dirname, '../../dist/index.html'));
-  });
-  
-  const port = process.env.PORT || 3000;
-  const server = http.createServer(app);
-  
-  server.listen(port, function(err){
-    if(err){
-      console.log(err);
-    }
-  });
+  console.log('mongodb is connected!');
 });
